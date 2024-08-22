95c4bdb98e48e848592546bc2c5fae15eea1aab0
DrDrProfRosenthal <samuel-rosenthal@web.de>
2024-08-22 17:13:19 +0200

    responsive updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
      
      <head>
-     <meta name="viewport" content="width=device-width, initial-scale=1" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.2" />
 
      <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com"  />
