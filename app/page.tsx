b77ddb57db6fcf84f9fcec0d6a523b1b0467eb7f
DrDrProfRosenthal <samuel-rosenthal@web.de>
2024-08-22 17:50:58 +0200

    responsive updates

--- app/page.tsx
+++ app/page.tsx
@@ -24,31 +24,29 @@ export default async function Page() {
  
 
       <div className="center-container">
-        <div className="prompt-container">
+        <div className="prompt-container bold-border-color ">
             
-          <div>
+              <div className="vocab-con my-4">
+                <span className="current-vocab dm-sans text-4xl bold-color" id="current-vocab">vorsichtig, achtsam, argwöhnisch  </span>
 
-            <div className="vocab-con ">
-                <span className="current-vocab dm-sans font-extrabold bold-color" id="current-vocab">vorsichtig, achtsam, argwöhnisch  </span>
-
-                  <div className='german-examplecontainer montserrat italic font-small' >
+                  <div className='german-examplecontainer montserrat italic text-base' >
                     <span className="english-example light-color" id="german-example"> "bleibt zwar weiter
                       <span className='germanExampleVocab counter-color'>&nbsp;vorsichtig&nbsp;</span>        
                       und kritisch, stellt jedoch fest, dass ein Pilotprojekt den Mitgliedstaaten dabei helfen [...]"</span>
                   </div>                
-             </div>
+              </div>
                
              
-                  <div className='vocab-con font-medium'>
-                    <span className='verbBox font-medium bold-color bold-weight hidden'>to</span>
-                    <input type="text" className="centered-input font-medium bold-color bold-border-color" placeholder="..." />
-                    <button  className="btn font-medium bold-color bold-border-color" id="myBtn" type="submit">Check!</button>
-                  </div>
+              <div className='vocab-con my-4'>
+                    <span className='verbBox text-base bold-color bold-weight hidden'>to</span>
+                    <input type="text" className="centered-input text-base bold-color bold-border-color" placeholder="..." />
+                    <button  className="btn text-base bold-color bold-border-color" id="myBtn" type="submit">Check!</button>
+              </div>
 
 
-                  <div className='english-examplecontainer vocab-con' >           
+              <div className='english-examplecontainer vocab-con my-4' >           
 
-                        <div id='hintContainer' className='flex-container font-small'>
+                        <div id='hintContainer' className='flex-container text-base'>
 
                             <span className='english-example montserrat italic light-color'> 
                               "Notes, while remaining 
@@ -60,16 +58,13 @@ export default async function Page() {
 
                            
                             <div>
-                              <button className='bold-color font-medium montserrat italic bold-border-color'  id="hintBtn">Reveal hint </button>
+                              <button className='bold-color text-base montserrat italic bold-border-color'  id="hintBtn">Reveal hint </button>
                             </div>
                             
-                        </div>
-                        
-                  </div>
+                        </div> 
 
-        
+              </div>
 
-          </div>
         </div>
     </div>
 
@@ -84,21 +79,21 @@ export default async function Page() {
     <div className='popupCon closed background-color bold-color'>
 
           
-                        <div className='popupForm lg:font-medium popupForm'>  
+                        <div className='popupForm lg:text-base popupForm'>  
 
                         
                                                   
                       <div className='flex flex-col'>
-                        <span className='dm-sans text-2xl lg:font-medium'>Input new words...</span>
-                        <input className='dm-sans text-4xl lg:font-medium' value={"wary"}></input>
+                        <span className='dm-sans text-2xl lg:text-base'>Input new words...</span>
+                        <input className='dm-sans text-4xl lg:text-base' value={"wary"}></input>
                       </div>                                
 
                       <form action={createInvoice}>                          
                         <div className='vocabInputField'>
                               <div>
                                   <div className=''>
-                                    <label className='text-basetext'>Example:&nbsp;</label>
-                                    <span className='text-basetext'>I am always wary when paying online with my credit card.</span>
+                                    <label className='text-base'>Example:&nbsp;</label>
+                                    <span className='text-base'>I am always wary when paying online with my credit card.</span>
                                   </div>
                                   <Pagination totalPages={2} />   
                               </div> 
@@ -109,8 +104,8 @@ export default async function Page() {
 
                               <div>
                                 <div className='inline-flexLG'>
-                                                      <label className='w-2 lg:w-10'>Beispiel:&nbsp;</label>
-                                                      <span className='lg:w-2 lg:w-10'>Wenn ich online mit Kreditkarte bezahle, bin ich immer vorsichtig.</span>
+                                                      <label className='text-base'>Beispiel:&nbsp;</label>
+                                                      <span className='text-base'>Wenn ich online mit Kreditkarte bezahle, bin ich immer vorsichtig.</span>
                                                     </div>
                                                     <Pagination totalPages={2} />   
                                 </div>   
@@ -128,7 +123,7 @@ export default async function Page() {
                             </div>
                                   
                             <div className='align-center'>
-                                <button type='submit' className='bold-color lg:font-medium montserrat italic bold-border-color text-base hover:bg-gray-100' id="addBtn" >Add vocab </button>
+                                <button type='submit' className='bold-color lg:text-base montserrat italic bold-border-color text-base hover:bg-gray-100' id="addBtn" >Add vocab </button>
                             </div>
 
 
@@ -151,63 +146,4 @@ export default async function Page() {
   );
 }
 
-  /*
-
-   <div className='popupForm lg:font-medium popupForm'>  
-
-   
-                            
-                  <div className='flex flex-col'>
-                    <span className='dm-sans text-2xl lg:font-medium'>Input new words...</span>
-                    <input className='dm-sans text-4xl lg:font-medium' value={"wary"}></input>
-                  </div>                                
-
-                  <form action={createInvoice}>                          
-                     <div className='vocabInputField'>
-                           <div>
-                               <div className=''>
-                                 <label className='text-basetext'>Example:&nbsp;</label>
-                                 <span className='text-basetext'>I am always wary when paying online with my credit card.</span>
-                               </div>
-                               <Pagination totalPages={2} />   
-                           </div> 
-                           
-                           <div>
-                              <span className='dm-sans text-4xl block'>wary</span>
-                          </div>
-
-                          <div>
-                            <div className='inline-flexLG'>
-                                                  <label className='w-2 lg:w-10'>Beispiel:&nbsp;</label>
-                                                  <span className='lg:w-2 lg:w-10'>Wenn ich online mit Kreditkarte bezahle, bin ich immer vorsichtig.</span>
-                                                </div>
-                                                <Pagination totalPages={2} />   
-                            </div>   
-
-                         <div>
-
-                                  <div className='flex flex-row'>
-                                      <span className='dm-sans text-xl  block'>vorsichtig</span>,&nbsp;
-                                      <span className='dm-sans text-xl block'>achtsam</span>,&nbsp;
-                                      <span className='dm-sans text-xl  block'>argwöhnisch</span>                            
-                                  </div>
-                                  <input className='dm-sans  hidden' value={"vorsichtig, achtsam, argwöhnisch"}></input>
-                                  <PencilIcon className="w-10 pencilBtn bold-color bold-border-color" />         
-                                          
-                                  
-                                
-                        </div>
-                              
-                        <div className='align-center'>
-                            <button type='submit' className='bold-color lg:font-medium montserrat italic bold-border-color text-base hover:bg-gray-100' id="addBtn" >Add vocab </button>
-                         </div>
-
-
-                     </div>
-                   </form>   
-
-       
-
-
-               </div>  
-                      */
+  
