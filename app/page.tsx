5ab114f771a95ccf23f6ddbd09a08b56024cc071
DrDrProfRosenthal <samuel-rosenthal@web.de>
2024-08-22 17:16:35 +0200

    responsive updates

--- app/page.tsx
+++ app/page.tsx
@@ -82,6 +82,12 @@ export default async function Page() {
 
       
     <div className='popupCon closed background-color bold-color'>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+
+
+
+
      <div className='popupForm lg:font-medium popupForm'>  
 
    
