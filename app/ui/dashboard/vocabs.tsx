5b907f8b6ebed01652eb720ec3eb43d9deb7d661
DrDrProfRosenthal <samuel-rosenthal@web.de>
2024-08-23 13:39:59 +0200

    form changes 9

--- app/ui/dashboard/vocabs.tsx
+++ app/ui/dashboard/vocabs.tsx
@@ -30,7 +30,7 @@ export default async function VocabsData({
             return (
                             
                 <p>
-                  {engvocabs.engVocab}
+                  {engvocabs.isVerb}
                 </p>
             )})}
 
